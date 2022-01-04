560a1b01c8c63c75b6bc1d622fa786015f92e533
vibin2002 <63181267+vibin2002@users.noreply.github.com>
2022-01-04 11:27:18 +0530

    Time: 9 ms (5.34%), Space: 63.8 MB (17.16%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -3,24 +3,20 @@ class Solution {
         int n = nums.length;
         if(nums.length <=1 || k%n == 0)
             return;
-        k = k%n;
+        // k = k%n;
         int prev = 0;
         int index = 0;
         int aux = nums[0];
-        // if(n%k == 0){
-        // } else {
-            for(int i=0;i<n;i++){
-                // System.out.println(Arrays.toString(nums) + aux);
-                int t = nums[(index+k)%n];
-                nums[(index+k)%n] = aux;
-                aux = t;
-                index = (index+k)%n;
-                if(index == prev){
-                    index++;
-                    prev = index;
-                    aux = nums[index];
-                }
+        for(int i=0;i<n;i++){
+            int t = nums[(index+k)%n];
+            nums[(index+k)%n] = aux;
+            aux = t;
+            index = (index+k)%n;
+            if(index == prev){
+                index++;
+                prev = index;
+                aux = nums[index];
             }
-        // }
+        }
     }
 }
