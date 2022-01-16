145131d834d978f31e9307379632cfa2386525f8
vibin2002 <63181267+vibin2002@users.noreply.github.com>
2022-01-16 12:13:11 +0530

    Time: 10 ms (74.76%), Space: 45.3 MB (37.92%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,15 +1,12 @@
 class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(char ch:s.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        int freq[] = new int[127];
+        for(int i=0;i<s.length();i++){
+            freq[(int)(s.charAt(i))]++;
         }
-        int i = 0;
-        for(char ch:s.toCharArray()){
-            if(map.get(ch) == 1)
+        for(int i=0;i<s.length();i++)
+            if(freq[(int)(s.charAt(i))] == 1)
                 return i;
-            i++;
-        }
         return -1;
     }
 }
