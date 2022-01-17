e10a4705b0d756055a1c0046c420cf4bcd0003fc
vibin2002 <63181267+vibin2002@users.noreply.github.com>
2022-01-17 09:25:51 +0530

    Time: 2 ms (30.18%), Space: 38.9 MB (15.00%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        Map<Character,String> map = new HashMap<>();
+        HashMap<Character,String> map = new HashMap<>();
         String arr[] = s.split(" ");
         if(pattern.length()!=arr.length) return false;
         for(int i=0;i<arr.length;i++){
