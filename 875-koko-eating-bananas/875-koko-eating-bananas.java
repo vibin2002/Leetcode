026c05847d814ad257a551cac9cac43c9b542778
vibin2002 <63181267+vibin2002@users.noreply.github.com>
2022-01-20 21:45:47 +0530

    Time: 48 ms (12.94%), Space: 52.3 MB (16.88%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1,r = 1;
+        for(int p:piles){
+            if(p>r) r = p;
+        }
+        while(l<r){
+            int mid = l+(r-l)/2;
+            int time = 0;
+            for(int p:piles){
+                time = time + (int)Math.ceil((double)p/mid);
+            }
+            
+            if(time<=h) 
+                r = mid;
+            else
+                l = mid+1;   
+        }
+        return r;
+    }
+}
