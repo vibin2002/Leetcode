ba88b8af8c67a5f036302bf602a936ede5b134b2
Vibin U <63181267+vibin2002@users.noreply.github.com>
2022-02-20 21:45:58 +0530

    Time: 8 ms (56.63%), Space: 46.2 MB (24.34%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -0,0 +1,31 @@
+class intervalsComparator implements Comparator<int[]> {
+    public int compare(int[] entry1, int[] entry2) {
+        if (entry1[0] > entry2[0]) {
+            return 1;
+        } else if(entry1[0] < entry2[0]) {
+            return -1;
+        } else if(entry1[1] > entry2[1]) {
+            return -1;
+        } else if(entry1[1] < entry2[1]) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
+
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+
+        Arrays.sort(intervals, new intervalsComparator());
+        int maxEnd = intervals[0][1];
+        int res = intervals.length;
+        for(int i = 1; i < intervals.length; i++) {
+            if(intervals[i][1] <= maxEnd) { 
+                res--; 
+            }
+            maxEnd = Math.max(maxEnd, intervals[i][1]); 
+        }
+        return res;
+    }
+}
