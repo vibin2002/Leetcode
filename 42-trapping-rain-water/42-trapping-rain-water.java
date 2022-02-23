3c41a687d33c7680f5ba8a9a083429ac0e0397e3
Vibin U <63181267+vibin2002@users.noreply.github.com>
2022-02-23 18:24:41 +0530

    Time: 1 ms (92.94%), Space: 46.3 MB (17.10%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,20 +1,23 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        int lmax[] = new int[n];
-        int rmax[] = new int[n];
-        int lm = height[0];
-        int rm = height[n-1];
-        for(int i=0;i<n;i++){
-            lm = Math.max(lm,height[i]);
-            rm = Math.max(rm,height[n-i-1]);
-            lmax[i] = lm;
-            rmax[n-i-1] = rm;
-        }
-        int sum = 0;
-        for(int i=0;i<n;i++){
-            int val = (Math.min(lmax[i],rmax[i]) - height[i]);
-            sum = sum+val;
+        if(n<=2)
+            return 0;
+        int lmax = height[0],rmax = height[n-1],sum = 0,l=0,r=n-2;
+        while(l<=r){
+            if(lmax<rmax){
+                if(height[l]>=lmax)
+                    lmax = height[l];
+                else
+                    sum = sum + lmax-height[l];
+                l++;
+            } else {
+                if(height[r]>=rmax)
+                    rmax = height[r];
+                else
+                    sum = sum + rmax-height[r];
+                r--;
+            }
         }
         return sum;
     }
