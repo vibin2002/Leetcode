9181597d7c91501e83eaf37515a9a38be5de6793
Vibin U <63181267+vibin2002@users.noreply.github.com>
2022-02-25 15:46:16 +0530

    Time: 1 ms (90.83%), Space: 40.1 MB (45.86%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -2,8 +2,6 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         String s1[] = version1.split("\\.");
         String s2[] = version2.split("\\.");
-        System.out.println(Arrays.toString(s1));
-        System.out.println(Arrays.toString(s2));
         int max = s1.length > s2.length ? s1.length : s2.length;
         int arr1[] = new int[max];
         int arr2[] = new int[max];
