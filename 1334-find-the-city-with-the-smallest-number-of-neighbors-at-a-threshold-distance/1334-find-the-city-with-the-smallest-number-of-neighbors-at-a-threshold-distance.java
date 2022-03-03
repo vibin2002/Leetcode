3a213dcbb44752ecf4643fccacdb1698af1b34e5
Vibin U <63181267+vibin2002@users.noreply.github.com>
2022-03-03 19:30:19 +0530

    Time: 16 ms (62.12%), Space: 45.9 MB (50.34%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -3,19 +3,15 @@ class Solution {
         int canvisit[] = new int[n];
         int max = Integer.MAX_VALUE;
         int graph[][] = new int[n][n];
-        // boolean isadded[][] = new boolean[n][n];
         for(int[] g:graph)
             Arrays.fill(g,Integer.MAX_VALUE);
         for(int i=0;i<n;i++){
             graph[i][i] = 0;
-            // isadded[i][i] = true;
         }
         for(int[] edge:edges){
             int u = edge[0],v = edge[1],w = edge[2];
             graph[u][v] = w;
             graph[v][u] = w;
-            // isadded[u][v] = true;
-            // isadded[v][u] = true;
         }
 
         for(int k=0;k<n;k++){
@@ -24,14 +20,11 @@ class Solution {
                     int otherdis = (graph[i][k]==Integer.MAX_VALUE || graph[k][j]==Integer.MAX_VALUE) ? Integer.MAX_VALUE : graph[i][k] + graph[k][j];
                     if(otherdis < graph[i][j] && otherdis <= distanceThreshold){
                         graph[i][j] = otherdis;
-                        // if(isadded[i][j] == false){
-                        //     canvisit[i]++;
-                        //     max = Math.max(max,canvisit[i]);
-                        // }
                     }
                 }
             }
         }
+        
         int res = -1;
         for(int i=0;i<n;i++){
             int count = 0;
@@ -46,9 +39,6 @@ class Solution {
                 res = i;
             }
         }
-        
-        for(int[] g:graph)
-            System.out.println(Arrays.toString(g));
         return res;
     }
 }
