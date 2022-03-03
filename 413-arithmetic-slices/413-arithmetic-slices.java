f88117b7bef025691bd2cb77422c26f142d74b3c
Vibin U <63181267+vibin2002@users.noreply.github.com>
2022-03-03 21:25:21 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (17.44%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -4,13 +4,15 @@ class Solution {
         if(len < 3)
             return 0;
         int res = 0;
-        int dp[] = new int[len];
+        int dp = 0;
         for(int i=1;i<len-1;i++){
             if(nums[i]-nums[i-1] == nums[i+1]-nums[i]){
-                dp[i+1] = dp[i]+1;
-                res += dp[i+1];
+                dp++;
+            } else {
+                res = res + (dp*(dp+1)/2);
+                dp = 0;
             }
         }
-        return res;
+        return res + (dp*(dp+1)/2);
     }
 }
