9e3a13443e2df605131f9ed7df571881d2c1db1e
Vibin U <63181267+vibin2002@users.noreply.github.com>
2022-03-18 15:13:29 +0530

    Time: 11 ms (6.79%), Space: 40.7 MB (62.21%) - LeetHub

---
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        while(true){
+            int size = q.size();
+            if(size == 0)
+                break;
+            int cnt = 0;
+            while(size-- > 0){
+                TreeNode curr = q.remove();
+                if(curr.left!=null){
+                    if(curr.left.val == x || curr.left.val == y)
+                        cnt++;
+                    q.add(curr.left);
+                }
+                if(curr.right!=null){
+                    if(curr.right.val == x || curr.right.val == y)
+                        cnt++;
+                    q.add(curr.right);
+                }
+                if(curr.left!=null && curr.right!=null){
+                    if(curr.left.val == x && curr.right.val == y){
+                        return false;
+                    }
+                    if(curr.left.val == y && curr.right.val == x){
+                        return false;
+                    }
+                }
+            }
+            for(TreeNode t:q)
+                System.out.print(t.val+" ");
+            System.out.println();
+            if(cnt == 2)
+                return true;
+        }
+        return false;
+    }
+}
