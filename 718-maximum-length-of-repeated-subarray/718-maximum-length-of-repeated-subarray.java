65d7be53271f3fc6b88e5b271c951025a36f2668
Vibin U <63181267+vibin2002@users.noreply.github.com>
2022-09-20 21:45:35 +0530

    Time: 79 ms (57.17%), Space: 77.8 MB (60.35%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -7,7 +7,7 @@ class Solution {
             for(int j=1;j<=l2;j++){
                 if(nums1[i-1] == nums2[j-1]){
                     dp[i][j] = 1+dp[i-1][j-1];
-                    max = Math.max(max,dp[i][j]);
+                    max = dp[i][j] > max ? dp[i][j] : max;
                 } 
             }
         }
