60fd09099a8915175bb8cdbbd380d67df63da2fc
Vibin U <63181267+vibin2002@users.noreply.github.com>
2022-10-09 18:50:00 +0530

    Time: 111 ms (11.39%), Space: 54.8 MB (18.25%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -2,28 +2,29 @@ class Solution {
     
     public int coinChange(int[] coins, int amount) {
         int dp[] = new int[amount+1];
-        Arrays.fill(dp,amount+1);
+        Arrays.fill(dp,Integer.MAX_VALUE);
         dp[0] = 0;
-        int res = find(coins,amount,dp,0,amount+1);
-        return res;
+        int res = find(coins,amount,dp,amount+1);
+        System.out.println(Arrays.toString(dp));
+        return (res >= amount+1) ? -1 : res ;
     }
     
     
-    private int find(int[] coins,int amount,int[] dp,int count,int total)
+    private int find(int[] coins,int amount,int[] dp,int total)
     {
         if(amount < 0){
             return total;
         }
-        if(dp[amount] != total)
+        if(dp[amount] != Integer.MAX_VALUE)
             return dp[amount];
-        int res = total;
         for(int coin : coins)
         {
-            int val = find(coins,amount-coin,dp,count+1,total);
-            if(val >= 0)
-                res = Math.min(res,val+1);
+            dp[amount] = Math.min(dp[amount],1 + find(coins,amount-coin,dp,total));
         }
-        return dp[amount] = (res == total) ? -1 : res;
+        return dp[amount];
     }
     
 }
+
+
+// [MAX,MAX,MAX,MAX]
