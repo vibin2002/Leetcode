44d2ec36a1220263356714eb581fcd2703a96ef1
Vibin U <63181267+vibin2002@users.noreply.github.com>
2022-10-23 12:25:20 +0530

    Time: 2 ms (97.99%), Space: 43.9 MB (91.81%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int freq[] = new int[nums.length];
-        for(int n:nums){
-            freq[n-1]++;
+        int duplicate = -1,missing = -1;
+        for(int n : nums){
+            n = Math.abs(n);
+            if(nums[n-1] < 0){
+                duplicate = n;
+                continue;
+            }
+            nums[n-1] = nums[n-1]*-1;
         }
-        int res[] = new int[2];
-        for(int i=0;i<nums.length;i++)
-        {
-            if(freq[i] == 2)
-                res[0] = i+1;
-            if(freq[i] == 0)
-                res[1] = i+1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] > 0)
+                missing = i+1;
         }
-        return res;
+        // System.out.println(Arrays.toString(nums) + " " +duplicate);
+        return new int[]{duplicate,missing};
     }
 }
