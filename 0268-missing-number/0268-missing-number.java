200449ecac61072b8163e40ea590b3940994d8ab
Vibin U <63181267+vibin2002@users.noreply.github.com>
2022-10-23 13:46:29 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (89.23%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for(int i : nums)
+            sum+=i;
+        int n = nums.length;
+        return n*(n+1)/2 - sum;
+    }
+}
