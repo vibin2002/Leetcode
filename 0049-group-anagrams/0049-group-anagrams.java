86c7fa59e1f50e1ad6b5a185a236da2005faf54b
Vibin U <63181267+vibin2002@users.noreply.github.com>
2022-10-28 07:24:51 +0530

    Time: 11 ms (85.78%), Space: 55.6 MB (66.55%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String str : strs)
+        {
+            char arr[] = str.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            map.putIfAbsent(key,new ArrayList<>());
+            map.get(key).add(str);
+        }
+        List<List<String>> res = new ArrayList<>(map.values());
+        return res;
+    }
+}
